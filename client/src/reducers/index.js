42002f6b1c5a802b985aee30da6381f7741b6e4d
Łukasz Leni Zembrowski <zembro@gmail.com>
2020-08-10 22:03:58 +0200

    Add reducers offer,cart

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,10 @@
 import { combineReducers } from "redux";
 import alert from "./alert";
+import offer from "./offer";
+import cart from "./cart";
 
 export default combineReducers({
   alert,
+  offer,
+  cart,
 });
