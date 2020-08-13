17bd516bd9113e147b3a6f856d916a73ceccbca1
Łukasz Leni Zembrowski <zembro@gmail.com>
2020-08-13 22:12:38 +0200

    Combine reducers

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import alert from "./alert";
 import offer from "./offer";
 import cart from "./cart";
+import toast from "./toast";
 
 export default combineReducers({
   alert,
   offer,
   cart,
+  toast,
 });
