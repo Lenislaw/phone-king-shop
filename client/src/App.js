1261355c8aa725a890348b7545a3c6a609c125b3
Łukasz Leni Zembrowski <zembro@gmail.com>
2020-09-07 18:49:44 +0200

    CSS and SCSS

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,7 @@ import React, { useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Offer from "./components/layout/Offer";
-import Delivery from "./components/layout/Delivery";
-import Contact from "./components/layout/Contact";
-import Login from "./components/layout/Login";
-import Register from "./components/layout/Register";
-import Wishlist from "./components/layout/Wishlist";
-import Cart from "./components/layout/Cart";
-import Details from "./components/layout/Details";
+import Routes from "./components/Routing/Routes";
 import Footer from "./components/layout/Footer";
 import M from "materialize-css/dist/js/materialize.min.js";
 // import "materialize-css/dist/css/materialize.min.css";
@@ -17,8 +11,14 @@ import "./App.css";
 //Redux
 import { Provider } from "react-redux";
 import store from "./store";
+import { loadUser } from "./actions/auth";
+import setAuthToken from "./utils/setAuthToken";
 
 function App() {
+  useEffect(() => {
+    setAuthToken(localStorage.token);
+    store.dispatch(loadUser());
+  }, []);
   useEffect(() => {
     // Init Materialize JS
     M.AutoInit();
@@ -28,16 +28,10 @@ function App() {
       <div className="App">
         <Router>
           <Navbar />
-
+         
           <Switch>
             <Route exact path="/" component={Offer} />
-            <Route exact path="/delivery" component={Delivery} />
-            <Route exact path="/details/:id" component={Details} />
-            <Route exact path="/contact" component={Contact} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/wishlist" component={Wishlist} />
-            <Route exact path="/cart" component={Cart} />
+            <Route component={Routes} />
           </Switch>
 
           <Footer></Footer>
